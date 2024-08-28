a6e6ec4b64903c9cba373f9d26afe49270373544
unknown <blandinedjoumsie@gmail.com>
2024-08-28 20:18:25 +0200

    adjusted the numeration once more and added validation to the tip demande

--- delivery-service.py
+++ delivery-service.py
@@ -13,7 +13,7 @@ while choice == "yes":
     if pizza_number <= len(list_of_available_pizza) and pizza_number > 0:
         order_list.append(list_of_available_pizza[pizza_number - 1])
         order = order + 1
-        print("Adding " + str(list_of_available_pizza[pizza_number]) + " to your order")
+        print("Adding " + str(list_of_available_pizza[pizza_number-1]) + " to your order")
     else:
         print("sorry pizza " + str(pizza_number) + " does not exist")
     choice = input("Do you want to add a pizza to your order? ")
@@ -22,8 +22,14 @@ print("you have ordered " + str(order) + " pizza")
 print("your order: " + str(order_list))
 Total_price = unit_price * len(order_list) 
 print("price " + str(Total_price)) 
-Tip = int(input("Do you want to add some tip? (0-25%)"))
-Tip_amount = (Tip/100)*Total_price
-Amount_to_pay = Tip_amount + Total_price
-print("Thanks, you have to pay " + str(Amount_to_pay))
+Tip_value = "invalid"
+while Tip_value == "invalid":
+    Tip = int(input("Do you want to add some tip? (0-25%)"))
+    Tip_amount = (Tip/100)*Total_price
+    Amount_to_pay = Tip_amount + Total_price
+    if Tip <= 25 and Tip > 0 :
+        print("Thanks, you have to pay " + str(Amount_to_pay))
+    else:
+        print("invalid tip")
+        Tip_value = "invalid"
 print("The pizza are on the way...")
