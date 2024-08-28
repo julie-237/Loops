e5b703924e7abed637f787d7eb60e1fb4975abe3
unknown <blandinedjoumsie@gmail.com>
2024-08-28 12:53:41 +0200

    added two number lists, one empty and the other with numbers, then doubled each number and added to the empty list

---
+++ lists&loops.py
@@ -0,0 +1,14 @@
+Month_list = ["January", "March", "May", "July", "August", "October", "December"]
+number_list_A =  [1, 5, 10, 15, 20, 25, 30]
+number_list_B =  []
+#for Month in Month_list:
+    
+    #print(Month)
+for number in number_list_A:
+    number_list_B.append(number*2)
+
+print(number_list_A)
+print(number_list_B)
+    
+#for i, month in enumerate(Month_list):
+    #print(str(i) + "; " + month)
